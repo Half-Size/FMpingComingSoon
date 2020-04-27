ffe2684de4ecf92838f755713bdc380bf609afb1
Billy Smallman-Köpf <wsmallmankoepf@outlook.com>
2020-04-27 15:55:50 +0100

    Complete

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 var form = document.notifyForm;
-form.setAttribute('novalidate', true);
 var emailBox = document.notifyForm.email;
+var errorMsg = document.getElementById('invalidEmail')
 
-function validate(field){
+//Turn off browser validation messages
+form.setAttribute('novalidate', true);
+
+//Function to define how page behaves if data input is invalid
+function error(message){
     event.preventDefault();
+    errorMsg.innerHTML = message;
+    emailBox.classList.add('inval');
+}
+
+//Function to validate input
+function validate(field){
+    //check the validity of the input and store it variable
     var validity = emailBox.validity
 
+    //if the box is empty prevent form submit and print error message
     if (validity.valueMissing){
-        document.getElementById('invalidEmail').innerHTML = "Whoops! It looks like you forgot to add your email";
-        document.notifyForm.email.classList.add("inval");
-    } else if (validity.typeMismatch) 
-        document.getElementById('invalidEmail').innerHTML = 'Please provide a valid email address';
-        document.notifyForm.email.classList.add("inval");
+        error("Whoops! It looks like you forgot to add your email");
+
+    //if the input is not a valid email address prevent form submit and print error
+    } else if (validity.typeMismatch) {
+        error("Please provide a valid email address");
+    }
 };
 
+//Listen for user attempt to submit email and run validate function on input
 form.addEventListener("submit", validate);
